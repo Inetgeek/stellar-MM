d1c142aecfb1eadf634da6e5b2bcb3f99e10f4ed
inetgeek <ranshens@foxmail.com>
2023-03-25 21:40:44 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+
+"""
+ @author: Colyn
+ @project: stellar-MM
+ @devtool: PyCharm
+ @date: 2023/3/25
+ @file: __init__.py.py
+"""
+from . import *
+
+__all__ = []
